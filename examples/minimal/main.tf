a8ff787be9b2d088706e0e2640eb87f8442c0dd1
tmknom <tmk.nom@gmail.com>
2018-11-14 22:17:37 +0900

    Implement minimal example

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,1 +1,18 @@
+module "alb" {
+  source = "../../"
+  name   = "minimal"
 
+  vpc_id  = "${module.vpc.vpc_id}"
+  subnets = ["${module.vpc.public_subnet_ids}"]
+}
+
+module "vpc" {
+  source  = "tmknom/vpc/aws"
+  version = "1.0.0"
+
+  cidr_block = "10.255.0.0/16"
+  name       = "minimal"
+
+  public_subnet_cidr_blocks = ["10.255.0.0/24", "10.255.1.0/24"]
+  public_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
+}
--- examples/minimal/outputs.tf
+++ examples/minimal/outputs.tf
@@ -1,1 +1,51 @@
+output "alb_id" {
+  value = "${module.alb.alb_id}"
+}
 
+output "alb_arn" {
+  value = "${module.alb.alb_arn}"
+}
+
+output "alb_arn_suffix" {
+  value = "${module.alb.alb_arn_suffix}"
+}
+
+output "alb_dns_name" {
+  value = "${module.alb.alb_dns_name}"
+}
+
+output "alb_zone_id" {
+  value = "${module.alb.alb_zone_id}"
+}
+
+output "security_group_id" {
+  value = "${module.alb.security_group_id}"
+}
+
+output "security_group_arn" {
+  value = "${module.alb.security_group_arn}"
+}
+
+output "security_group_vpc_id" {
+  value = "${module.alb.security_group_vpc_id}"
+}
+
+output "security_group_owner_id" {
+  value = "${module.alb.security_group_owner_id}"
+}
+
+output "security_group_name" {
+  value = "${module.alb.security_group_name}"
+}
+
+output "security_group_description" {
+  value = "${module.alb.security_group_description}"
+}
+
+output "security_group_ingress" {
+  value = "${module.alb.security_group_ingress}"
+}
+
+output "security_group_egress" {
+  value = "${module.alb.security_group_egress}"
+}
