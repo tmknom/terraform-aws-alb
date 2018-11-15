7ce297fdc445cf0eeb70e9c1123d95bbd227b3b9
tmknom <tmk.nom@gmail.com>
2018-11-15 11:01:31 +0900

    Update minimal example to reflect changes access logs

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -2,6 +2,7 @@ module "alb" {
   source = "../../"
   name   = "minimal"
 
+  access_logs_bucket = "${module.s3_lb_log.s3_bucket_id}"
   vpc_id  = "${module.vpc.vpc_id}"
   subnets = ["${module.vpc.public_subnet_ids}"]
 }
@@ -14,3 +15,18 @@ module "vpc" {
   public_subnet_cidr_blocks = ["10.255.0.0/24", "10.255.1.0/24"]
   public_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
 }
+
+module "s3_lb_log" {
+  source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/1.0.0"
+  name                  = "s3-lb-log-${data.aws_caller_identity.current.account_id}"
+  logging_target_bucket = "${module.s3_access_log.s3_bucket_id}"
+  force_destroy         = true
+}
+
+module "s3_access_log" {
+  source        = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=tags/1.0.0"
+  name          = "s3-access-log-${data.aws_caller_identity.current.account_id}"
+  force_destroy = true
+}
+
+data "aws_caller_identity" "current" {}
