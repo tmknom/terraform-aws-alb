87cd3acf2ab710425d915b7497df508f12ef46d2
tmknom <tmk.nom@gmail.com>
2018-11-15 10:44:58 +0900

    Add access logs

--- variables.tf
+++ variables.tf
@@ -8,6 +8,11 @@ variable "subnets" {
   description = "A list of subnet IDs to attach to the LB. At least two subnets in two different Availability Zones must be specified."
 }
 
+variable "access_logs_bucket" {
+  type        = "string"
+  description = "The S3 bucket name to store the logs in. Even if access_logs_enabled set false, you need to specify the valid bucket to access_logs_bucket."
+}
+
 variable "vpc_id" {
   type        = "string"
   description = "VPC Id to associate with ALB."
@@ -41,6 +46,18 @@ variable "ip_address_type" {
   default     = "ipv4"
   type        = "string"
   description = "The type of IP addresses used by the subnets for your load balancer. The possible values are ipv4 and dualstack."
+}
+
+variable "access_logs_prefix" {
+  default     = ""
+  type        = "string"
+  description = "The S3 bucket prefix. Logs are stored in the root if not configured."
+}
+
+variable "access_logs_enabled" {
+  default     = true
+  type        = "string"
+  description = "Boolean to enable / disable access_logs."
 }
 
 variable "https_port" {
