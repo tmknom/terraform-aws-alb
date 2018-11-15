7da9ab2fc1ac48c41ec7ff7583c3948bf4329457
tmknom <tmk.nom@gmail.com>
2018-11-15 11:02:07 +0900

    Foramt code

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -3,15 +3,14 @@ module "alb" {
   name   = "minimal"
 
   access_logs_bucket = "${module.s3_lb_log.s3_bucket_id}"
-  vpc_id  = "${module.vpc.vpc_id}"
-  subnets = ["${module.vpc.public_subnet_ids}"]
+  vpc_id             = "${module.vpc.vpc_id}"
+  subnets            = ["${module.vpc.public_subnet_ids}"]
 }
 
 module "vpc" {
-  source     = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
-  cidr_block = "10.255.0.0/16"
-  name       = "minimal"
-
+  source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
+  cidr_block                = "10.255.0.0/16"
+  name                      = "minimal"
   public_subnet_cidr_blocks = ["10.255.0.0/24", "10.255.1.0/24"]
   public_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
 }
