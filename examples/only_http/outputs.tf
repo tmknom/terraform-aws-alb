bc95397063100212c426302dbedbde8bdf7a2620
tmknom <tmk.nom@gmail.com>
2018-11-23 10:49:02 +0900

    Implement only http example

---
+++ examples/only_http/outputs.tf
@@ -0,0 +1,67 @@
+output "alb_id" {
+  value = "${module.alb.alb_id}"
+}
+
+output "alb_arn" {
+  value = "${module.alb.alb_arn}"
+}
+
+output "alb_arn_suffix" {
+  value = "${module.alb.alb_arn_suffix}"
+}
+
+output "alb_dns_name" {
+  value = "${module.alb.alb_dns_name}"
+}
+
+output "alb_zone_id" {
+  value = "${module.alb.alb_zone_id}"
+}
+
+output "https_alb_listener_id" {
+  value = "${module.alb.https_alb_listener_id}"
+}
+
+output "https_alb_listener_arn" {
+  value = "${module.alb.https_alb_listener_arn}"
+}
+
+output "http_alb_listener_id" {
+  value = "${module.alb.http_alb_listener_id}"
+}
+
+output "http_alb_listener_arn" {
+  value = "${module.alb.http_alb_listener_arn}"
+}
+
+output "security_group_id" {
+  value = "${module.alb.security_group_id}"
+}
+
+output "security_group_arn" {
+  value = "${module.alb.security_group_arn}"
+}
+
+output "security_group_vpc_id" {
+  value = "${module.alb.security_group_vpc_id}"
+}
+
+output "security_group_owner_id" {
+  value = "${module.alb.security_group_owner_id}"
+}
+
+output "security_group_name" {
+  value = "${module.alb.security_group_name}"
+}
+
+output "security_group_description" {
+  value = "${module.alb.security_group_description}"
+}
+
+output "security_group_ingress" {
+  value = "${module.alb.security_group_ingress}"
+}
+
+output "security_group_egress" {
+  value = "${module.alb.security_group_egress}"
+}
