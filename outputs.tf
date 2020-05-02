0303ec2d240ad0cb16917a7a5bb97601699a5099
tmknom <tmk.nom@gmail.com>
2020-05-02 22:22:31 +0900

    Upgraded module to support Terraform 0.12

--- outputs.tf
+++ outputs.tf
@@ -1,139 +1,139 @@
 output "alb_id" {
-  value       = "${join("", aws_lb.default.*.id)}"
+  value       = join("", aws_lb.default.*.id)
   description = "The ARN of the load balancer (matches arn)."
 }
 
 output "alb_arn" {
-  value       = "${join("", aws_lb.default.*.arn)}"
+  value       = join("", aws_lb.default.*.arn)
   description = "The ARN of the load balancer (matches id)."
 }
 
 output "alb_arn_suffix" {
-  value       = "${join("", aws_lb.default.*.arn_suffix)}"
+  value       = join("", aws_lb.default.*.arn_suffix)
   description = "The ARN suffix for use with CloudWatch Metrics."
 }
 
 output "alb_dns_name" {
-  value       = "${join("", aws_lb.default.*.dns_name)}"
+  value       = join("", aws_lb.default.*.dns_name)
   description = "The DNS name of the load balancer."
 }
 
 output "alb_zone_id" {
-  value       = "${join("", aws_lb.default.*.zone_id)}"
+  value       = join("", aws_lb.default.*.zone_id)
   description = "The canonical hosted zone ID of the load balancer (to be used in a Route 53 Alias record)."
 }
 
 output "https_alb_listener_id" {
-  value       = "${join("", aws_lb_listener.https.*.id)}"
+  value       = join("", aws_lb_listener.https.*.id)
   description = "The ARN of the HTTPS listener (matches arn)"
 }
 
 output "https_alb_listener_arn" {
-  value       = "${join("", aws_lb_listener.https.*.arn)}"
+  value       = join("", aws_lb_listener.https.*.arn)
   description = "The ARN of the HTTPS listener (matches id)"
 }
 
 output "http_alb_listener_id" {
-  value       = "${join("", aws_lb_listener.http.*.id)}"
+  value       = join("", aws_lb_listener.http.*.id)
   description = "The ARN of the HTTP listener (matches arn)"
 }
 
 output "http_alb_listener_arn" {
-  value       = "${join("", aws_lb_listener.http.*.arn)}"
+  value       = join("", aws_lb_listener.http.*.arn)
   description = "The ARN of the HTTP listener (matches id)"
 }
 
 output "redirect_http_to_https_alb_listener_id" {
-  value       = "${join("", aws_lb_listener.redirect_http_to_https.*.id)}"
+  value       = join("", aws_lb_listener.redirect_http_to_https.*.id)
   description = "The ARN of the HTTP listener of HTTPS redirect (matches arn)"
 }
 
 output "redirect_http_to_https_alb_listener_arn" {
-  value       = "${join("", aws_lb_listener.redirect_http_to_https.*.arn)}"
+  value       = join("", aws_lb_listener.redirect_http_to_https.*.arn)
   description = "The ARN of the HTTP listener of HTTPS redirect (matches id)"
 }
 
 output "alb_target_group_id" {
-  value       = "${join("", aws_lb_target_group.default.*.id)}"
+  value       = join("", aws_lb_target_group.default.*.id)
   description = "The ARN of the Target Group (matches arn)"
 }
 
 output "alb_target_group_arn" {
-  value       = "${join("", aws_lb_target_group.default.*.arn)}"
+  value       = join("", aws_lb_target_group.default.*.arn)
   description = "The ARN of the Target Group (matches id)"
 }
 
 output "alb_target_group_arn_suffix" {
-  value       = "${join("", aws_lb_target_group.default.*.arn_suffix)}"
+  value       = join("", aws_lb_target_group.default.*.arn_suffix)
   description = "The ARN suffix for use with CloudWatch Metrics."
 }
 
 output "alb_target_group_name" {
-  value       = "${join("", aws_lb_target_group.default.*.name)}"
+  value       = join("", aws_lb_target_group.default.*.name)
   description = "The name of the Target Group."
 }
 
 output "alb_target_group_port" {
-  value       = "${join("", aws_lb_target_group.default.*.port)}"
+  value       = join("", aws_lb_target_group.default.*.port)
   description = "The port of the Target Group."
 }
 
 output "https_alb_listener_rule_id" {
-  value       = "${join("", aws_lb_listener_rule.https.*.id)}"
+  value       = join("", aws_lb_listener_rule.https.*.id)
   description = "The ARN of the HTTPS rule (matches arn)"
 }
 
 output "https_alb_listener_rule_arn" {
-  value       = "${join("", aws_lb_listener_rule.https.*.arn)}"
+  value       = join("", aws_lb_listener_rule.https.*.arn)
   description = "The ARN of the HTTPS rule (matches id)"
 }
 
 output "http_alb_listener_rule_id" {
-  value       = "${join("", aws_lb_listener_rule.http.*.id)}"
+  value       = join("", aws_lb_listener_rule.http.*.id)
   description = "The ARN of the HTTP rule (matches arn)"
 }
 
 output "http_alb_listener_rule_arn" {
-  value       = "${join("", aws_lb_listener_rule.http.*.arn)}"
+  value       = join("", aws_lb_listener_rule.http.*.arn)
   description = "The ARN of the HTTP rule (matches id)"
 }
 
 output "security_group_id" {
-  value       = "${join("", aws_security_group.default.*.id)}"
+  value       = join("", aws_security_group.default.*.id)
   description = "The ID of the alb security group."
 }
 
 output "security_group_arn" {
-  value       = "${join("", aws_security_group.default.*.arn)}"
+  value       = join("", aws_security_group.default.*.arn)
   description = "The ARN of the alb security group."
 }
 
 output "security_group_vpc_id" {
-  value       = "${join("", aws_security_group.default.*.vpc_id)}"
+  value       = join("", aws_security_group.default.*.vpc_id)
   description = "The VPC ID of the alb security group."
 }
 
 output "security_group_owner_id" {
-  value       = "${join("", aws_security_group.default.*.owner_id)}"
+  value       = join("", aws_security_group.default.*.owner_id)
   description = "The owner ID of the alb security group."
 }
 
 output "security_group_name" {
-  value       = "${join("", aws_security_group.default.*.name)}"
+  value       = join("", aws_security_group.default.*.name)
   description = "The name of the alb security group."
 }
 
 output "security_group_description" {
-  value       = "${join("", aws_security_group.default.*.description)}"
+  value       = join("", aws_security_group.default.*.description)
   description = "The description of the alb security group."
 }
 
 output "security_group_ingress" {
-  value       = "${aws_security_group.default.*.ingress}"
+  value       = aws_security_group.default.*.ingress
   description = "The ingress rules of the alb security group."
 }
 
 output "security_group_egress" {
-  value       = "${aws_security_group.default.*.egress}"
+  value       = aws_security_group.default.*.egress
   description = "The egress rules of the alb security group."
 }
--- variables.tf
+++ variables.tf
@@ -1,235 +1,235 @@
 variable "name" {
-  type        = "string"
+  type        = string
   description = "The name of the LB. This name must be unique within your AWS account."
 }
 
 variable "subnets" {
-  type        = "list"
+  type        = list(string)
   description = "A list of subnet IDs to attach to the LB. At least two subnets in two different Availability Zones must be specified."
 }
 
 variable "access_logs_bucket" {
-  type        = "string"
+  type        = string
   description = "The S3 bucket name to store the logs in. Even if access_logs_enabled set false, you need to specify the valid bucket to access_logs_bucket."
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC Id to associate with ALB."
 }
 
 variable "internal" {
   default     = false
-  type        = "string"
+  type        = string
   description = "If true, the LB will be internal."
 }
 
 variable "idle_timeout" {
   default     = 60
-  type        = "string"
+  type        = string
   description = "The time in seconds that the connection is allowed to be idle."
 }
 
 variable "enable_deletion_protection" {
   default     = true
-  type        = "string"
+  type        = string
   description = "If true, deletion of the load balancer will be disabled via the AWS API."
 }
 
 variable "enable_http2" {
   default     = true
-  type        = "string"
+  type        = string
   description = "Indicates whether HTTP/2 is enabled in application load balancers."
 }
 
 variable "ip_address_type" {
   default     = "ipv4"
-  type        = "string"
+  type        = string
   description = "The type of IP addresses used by the subnets for your load balancer. The possible values are ipv4 and dualstack."
 }
 
 variable "access_logs_prefix" {
   default     = ""
-  type        = "string"
+  type        = string
   description = "The S3 bucket prefix. Logs are stored in the root if not configured."
 }
 
 variable "access_logs_enabled" {
   default     = true
-  type        = "string"
+  type        = string
   description = "Boolean to enable / disable access_logs."
 }
 
 variable "enable_https_listener" {
   default     = true
-  type        = "string"
+  type        = string
   description = "If true, the HTTPS listener will be created."
 }
 
 variable "enable_http_listener" {
   default     = true
-  type        = "string"
+  type        = string
   description = "If true, the HTTP listener will be created."
 }
 
 variable "enable_redirect_http_to_https_listener" {
   default     = true
-  type        = "string"
+  type        = string
   description = "If true, the HTTP listener of HTTPS redirect will be created."
 }
 
 variable "ssl_policy" {
   default     = "ELBSecurityPolicy-2016-08"
-  type        = "string"
+  type        = string
   description = "The name of the SSL Policy for the listener. Required if protocol is HTTPS."
 }
 
 variable "certificate_arn" {
   default     = ""
-  type        = "string"
+  type        = string
   description = "The ARN of the default SSL server certificate. Exactly one certificate is required if the protocol is HTTPS."
 }
 
 variable "https_port" {
   default     = 443
-  type        = "string"
+  type        = string
   description = "The HTTPS port."
 }
 
 variable "http_port" {
   default     = 80
-  type        = "string"
+  type        = string
   description = "The HTTP port."
 }
 
 variable "fixed_response_content_type" {
   default     = "text/plain"
-  type        = "string"
+  type        = string
   description = "The content type. Valid values are text/plain, text/css, text/html, application/javascript and application/json."
 }
 
 variable "fixed_response_message_body" {
   default     = "404 Not Found"
-  type        = "string"
+  type        = string
   description = "The message body."
 }
 
 variable "fixed_response_status_code" {
   default     = "404"
-  type        = "string"
+  type        = string
   description = "The HTTP response code. Valid values are 2XX, 4XX, or 5XX."
 }
 
 variable "target_group_port" {
   default     = "80"
-  type        = "string"
+  type        = string
   description = "The port on which targets receive traffic, unless overridden when registering a specific target."
 }
 
 variable "target_group_protocol" {
   default     = "HTTP"
-  type        = "string"
+  type        = string
   description = "The protocol to use for routing traffic to the targets. Should be one of HTTP or HTTPS."
 }
 
 variable "target_type" {
   default     = "ip"
-  type        = "string"
+  type        = string
   description = "The type of target that you must specify when registering targets with this target group. The possible values are instance or ip."
 }
 
 variable "deregistration_delay" {
   default     = "300"
-  type        = "string"
+  type        = string
   description = "The amount time for the load balancer to wait before changing the state of a deregistering target from draining to unused."
 }
 
 variable "slow_start" {
   default     = "0"
-  type        = "string"
+  type        = string
   description = "The amount time for targets to warm up before the load balancer sends them a full share of requests."
 }
 
 variable "health_check_path" {
   default     = "/"
-  type        = "string"
+  type        = string
   description = "The destination for the health check request."
 }
 
 variable "health_check_healthy_threshold" {
   default     = "5"
-  type        = "string"
+  type        = string
   description = "The number of consecutive health checks successes required before considering an unhealthy target healthy."
 }
 
 variable "health_check_unhealthy_threshold" {
   default     = "2"
-  type        = "string"
+  type        = string
   description = "The number of consecutive health check failures required before considering the target unhealthy."
 }
 
 variable "health_check_timeout" {
   default     = "5"
-  type        = "string"
+  type        = string
   description = "The amount of time, in seconds, during which no response means a failed health check."
 }
 
 variable "health_check_interval" {
   default     = "30"
-  type        = "string"
+  type        = string
   description = "The approximate amount of time, in seconds, between health checks of an individual target."
 }
 
 variable "health_check_matcher" {
   default     = "200"
-  type        = "string"
+  type        = string
   description = "The HTTP codes to use when checking for a successful response from a target."
 }
 
 variable "health_check_port" {
   default     = "traffic-port"
-  type        = "string"
+  type        = string
   description = "The port to use to connect with the target."
 }
 
 variable "health_check_protocol" {
   default     = "HTTP"
-  type        = "string"
+  type        = string
   description = "The protocol to use to connect with the target."
 }
 
 variable "listener_rule_priority" {
   default     = 50000
-  type        = "string"
+  type        = string
   description = "The priority for the rule between 1 and 50000."
 }
 
 variable "listener_rule_condition_field" {
   default     = "path-pattern"
-  type        = "string"
+  type        = string
   description = "The name of the field. Must be one of path-pattern for path based routing or host-header for host based routing."
 }
 
 variable "listener_rule_condition_values" {
   default     = ["/*"]
-  type        = "list"
+  type        = list(string)
   description = "The path patterns to match. A maximum of 1 can be defined."
 }
 
 variable "ingress_cidr_blocks" {
   default     = ["0.0.0.0/0"]
-  type        = "list"
+  type        = list(string)
   description = "List of Ingress CIDR blocks."
 }
 
 variable "tags" {
   default     = {}
-  type        = "map"
+  type        = map(string)
   description = "A mapping of tags to assign to all resources."
 }
 
 variable "enabled" {
   default     = true
-  type        = "string"
+  type        = string
   description = "Set to false to prevent the module from creating anything."
 }
