430a9188fe4ff5f42b50383c0f1c8ff37b59d781
tmknom <tmk.nom@gmail.com>
2020-05-02 22:39:48 +0900

    Bump modules for examples

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,7 +2,7 @@ module "alb" {
   source             = "../../"
   name               = "example"
   vpc_id             = module.vpc.vpc_id
-  subnets            = [module.vpc.public_subnet_ids]
+  subnets            = module.vpc.public_subnet_ids
   access_logs_bucket = module.s3_lb_log.s3_bucket_id
   certificate_arn    = module.certificate.acm_certificate_arn
 
@@ -52,28 +52,28 @@ module "alb" {
 }
 
 module "certificate" {
-  source      = "git::https://github.com/tmknom/terraform-aws-acm-certificate.git?ref=tags/1.0.0"
+  source      = "git::https://github.com/tmknom/terraform-aws-acm-certificate.git?ref=tags/2.0.0"
   domain_name = "alb.${local.domain_name}"
   zone_id     = data.aws_route53_zone.default.id
 }
 
 module "vpc" {
-  source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
+  source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/2.0.1"
   cidr_block                = local.cidr_block
   name                      = "alb"
   public_subnet_cidr_blocks = [cidrsubnet(local.cidr_block, 8, 0), cidrsubnet(local.cidr_block, 8, 1)]
-  public_availability_zones = [data.aws_availability_zones.available.names]
+  public_availability_zones = data.aws_availability_zones.available.names
 }
 
 module "s3_lb_log" {
-  source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/1.0.0"
+  source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/2.0.0"
   name                  = "s3-lb-log-alb-${data.aws_caller_identity.current.account_id}"
   logging_target_bucket = module.s3_access_log.s3_bucket_id
   force_destroy         = true
 }
 
 module "s3_access_log" {
-  source        = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=tags/1.0.0"
+  source        = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=tags/2.0.0"
   name          = "s3-access-log-alb-${data.aws_caller_identity.current.account_id}"
   force_destroy = true
 }
