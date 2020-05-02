857670d541b43564e23c23fc0a4e6369671ebcd5
tmknom <tmk.nom@gmail.com>
2020-05-02 22:28:00 +0900

    Upgraded examples/only_http to support Terraform 0.12

--- examples/only_http/main.tf
+++ examples/only_http/main.tf
@@ -1,9 +1,9 @@
 module "alb" {
   source             = "../../"
   name               = "example"
-  vpc_id             = "${module.vpc.vpc_id}"
-  subnets            = ["${module.vpc.public_subnet_ids}"]
-  access_logs_bucket = "${module.s3_lb_log.s3_bucket_id}"
+  vpc_id             = module.vpc.vpc_id
+  subnets            = [module.vpc.public_subnet_ids]
+  access_logs_bucket = module.s3_lb_log.s3_bucket_id
 
   # NOTE: You can provision only HTTP, if enable_https_listener is set to false.
   enable_https_listener = false
@@ -14,16 +14,16 @@ module "alb" {
 
 module "vpc" {
   source                    = "git::https://github.com/tmknom/terraform-aws-vpc.git?ref=tags/1.0.0"
-  cidr_block                = "${local.cidr_block}"
+  cidr_block                = local.cidr_block
   name                      = "alb"
-  public_subnet_cidr_blocks = ["${cidrsubnet(local.cidr_block, 8, 0)}", "${cidrsubnet(local.cidr_block, 8, 1)}"]
-  public_availability_zones = ["${data.aws_availability_zones.available.names}"]
+  public_subnet_cidr_blocks = [cidrsubnet(local.cidr_block, 8, 0), cidrsubnet(local.cidr_block, 8, 1)]
+  public_availability_zones = [data.aws_availability_zones.available.names]
 }
 
 module "s3_lb_log" {
   source                = "git::https://github.com/tmknom/terraform-aws-s3-lb-log.git?ref=tags/1.0.0"
   name                  = "s3-lb-log-alb-${data.aws_caller_identity.current.account_id}"
-  logging_target_bucket = "${module.s3_access_log.s3_bucket_id}"
+  logging_target_bucket = module.s3_access_log.s3_bucket_id
   force_destroy         = true
 }
 
